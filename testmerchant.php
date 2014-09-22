854ae6c2aa9c3465320b3fc09ef57fb137b32885
Marin Ravenel <kalasasta@gmail.com>
2014-09-22 10:30:49 +0000

    Test

--- testmerchant.php
+++ testmerchant.php
@@ -6,3 +6,6 @@ require_once "iceberg.php";
 $Icbrg = new Iceberg(array("username"=>"mravenel", "accessToken"=>"e405da7aa2751d695ea420987e5af8759fb9e6ee", "sandbox"=>"true"));
 $Merchant = new Merchant();
 echo var_export($Merchant->get(15), true);
+
+
+
