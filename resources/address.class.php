2e17ce561cf701d3725fe41550f671a47da76296
Marin Ravenel <kalasasta@gmail.com>
2014-09-29 12:35:05 +0200

    Modified Address class

--- resources/address.class.php
+++ resources/address.class.php
@@ -26,40 +26,8 @@ class Address extends Resource
         zipcode: integer
      */
 
-    public function __construct()
-    {
-
-        parent::__construct();
-    }
 }
 
 class MerchantAddress extends Resource
 {
-    /* Params:
-        address: string
-        address2: string
-        city: string
-        company: string
-        country: string
-        default_billing: boolean
-        default_shipping: boolean
-        digicode: string
-        first_name: string
-        floor: string
-        last_name: string
-        name: string
-        phone: string
-        state: string
-        status:
-          0: Inactive address
-          10: Active address
-          90: Hidden address
-        zipcode: integer
-     */
-
-    public function __construct()
-    {
-
-        parent::__construct();
-    }
 }
