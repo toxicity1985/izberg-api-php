33d3b9497b684a00b6b6957d2cafe522d6133e48
Marin Ravenel <kalasasta@gmail.com>
2014-09-29 17:40:39 +0200

    deleted testmerchant.php from branch

--- testmerchant.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-require_once "iceberg.php";
-
-$Icbrg = new Iceberg(array("username"=>"mravenel", "accessToken"=>"e405da7aa2751d695ea420987e5af8759fb9e6ee", "sandbox"=>"true"));
-$Merchant = $Icbrg->make("Merchant", 15);
-echo var_export($Merchant->getCurrent(), true);
