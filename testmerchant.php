f4788e9bcf5eb608473ffdc3805305f57dd324c6
Marin Ravenel <kalasasta@gmail.com>
2014-09-29 17:39:55 +0200

    Updated gitignore

--- testmerchant.php
+++ testmerchant.php
@@ -3,7 +3,6 @@
 
 require_once "iceberg.php";
 
-
 $Icbrg = new Iceberg(array("username"=>"mravenel", "accessToken"=>"e405da7aa2751d695ea420987e5af8759fb9e6ee", "sandbox"=>"true"));
 $Merchant = $Icbrg->make("Merchant", 15);
 echo var_export($Merchant->getCurrent(), true);
