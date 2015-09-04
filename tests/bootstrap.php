175d6b885c330b57b61c25c53fc2991a96048769
Sébastien Fieloux <sebfie@yahoo.fr>
2015-09-04 15:25:46 +0200

    Fix for travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,3 @@ require_once "base.php";
 require_once __DIR__ . '/../vendor/autoload.php';
 
 \VCR\VCR::turnOn();
-# Turn VCR on before to load izberg if we want to catch curls
-require_once "lib/izberg.php";
