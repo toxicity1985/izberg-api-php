9f4045bc4cca9594a49ad97c1d8d7fe4f149d7e2
Sébastien Fieloux <sebfie@yahoo.fr>
2016-06-27 23:56:42 +0200

    Add a method on payment to make is as "pending"

--- lib/Resource/Payment.php
+++ lib/Resource/Payment.php
@@ -4,5 +4,13 @@ use Izberg\Resource;
 
 class Payment extends Resource
 {
-
+  /**
+  * Make pending payment Authorization
+  * @return Payment
+  */
+  public function pending(){
+		$response = parent::$Izberg->Call( $this->getName() . '/'. $this->id . "/pending_authorization/", 'POST', null, 'Content-Type: application/json');
+		$this->hydrate($response);
+		return $this;
+	}
 }
