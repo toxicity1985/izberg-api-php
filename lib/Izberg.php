ba24ac1be5f6eba83effa8615c43df7437d35019
kevlee <kevlee681@gmail.com>
2016-12-15 18:16:09 +0100

    Call fix content type check json_encode post

--- lib/Izberg.php
+++ lib/Izberg.php
@@ -661,7 +661,7 @@ class Izberg
 	*/
 	public function Call($path, $method = 'GET', $params = null, $accept_type = 'Accept: application/json', $content_type = 'Content-Type: application/json; charset=UTF-8', $output_file = null)
 	{
-		if (!is_null($params) && is_array($params) && $accept_type == "Content-Type: application/json")
+		if (!is_null($params) && is_array($params) && $content_type == "Content-Type: application/json; charset=UTF-8")
 		{
 			$paramString = json_encode($params);
 		}
